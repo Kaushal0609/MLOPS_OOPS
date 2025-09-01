44a2de3b6beb44e50b6199565c3ab49250df2761
Kaushal0609 <kaushalkathiriya1628@gmail.com>
2025-09-01 17:19:59 +0530

    Encapsulation, Getter Setter and Static method

--- rough.py
+++ rough.py
@@ -1,3 +1,16 @@
 from oops_proj import chatbook
 
 user1 = chatbook()
+print(user1.id)
+
+# Getter and Setter
+# print(user1.get_name())
+# user1.set_name("KING")
+# print(user1.get_name())
+
+# Static method
+
+# chatbook.set_id(10)
+
+# user2 = chatbook()
+# print(user2.id)
